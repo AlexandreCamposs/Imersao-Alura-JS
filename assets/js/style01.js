acf1a4a25ac8adf6f6e2d35d24340787e0b2509b
AlexandreCamposs <89165912+AlexandreCamposs@users.noreply.github.com>
2022-03-10 14:28:27 -0300

    att

---
+++ assets/js/style01.js
@@ -0,0 +1,18 @@
+var nome = "Alexandre"
+consolo.log("Alexandre")
+var notaPrimeiroBimestre = 9
+
+var notaSegundoBimestre = 7
+
+var notaTerceiroBimestre = 4
+
+var notaQuartoBimestre = 2
+
+var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4
+
+var notaFixa = notaFinal.toFixed(1)
+
+console.log("Bem vindo" + nome)
+
+console.log(notaFinal)
+
